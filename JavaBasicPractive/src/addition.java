ca56650487bc1835cc572d165ba974a1ed70fa3c
abhisheksaklani28 <asaklani008@gmail.com>
2024-01-04 22:01:41 +0530

    hello

---
+++ JavaBasicPractive/src/addition.java
@@ -0,0 +1,17 @@
+
+public class addition 
+{
+	public void add(int a,int b)
+	{
+		int c;
+		c=a+b;
+		System.out.println("addition "+c);
+	}
+	
+public static void main(String[] args) 
+{
+	addition abc=new addition() ;
+	abc.add(10, 20);
+	
+}
+}
